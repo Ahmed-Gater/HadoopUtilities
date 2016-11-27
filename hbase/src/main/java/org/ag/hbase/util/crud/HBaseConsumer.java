2c020302147dc1246bd59d51da035e1029f3ab4b
ahmed.gater <ahmed.gater@gmail.com>
2016-11-27 16:07:22 +0100

    Adding HbaseConsumerTest and HBaseConsumer

--- hbase/src/main/java/org/ag/hbase/util/crud/HBaseConsumer.java
+++ hbase/src/main/java/org/ag/hbase/util/crud/HBaseConsumer.java
@@ -1,21 +1,24 @@
 package org.ag.hbase.util.crud;
 
 import org.ag.hbase.conf.HBaseConnectionBuilder;
+import org.ag.hbase.util.admin.AdminUtils;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellScanner;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.Pair;
 import org.javatuples.Quartet;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by ahmed.gater on 23/11/2016.
@@ -23,12 +26,12 @@ import java.util.List;
 public class HBaseConsumer {
 
     Table table;
-
+    Set<String> columnFamilies ;
     public HBaseConsumer(Connection conn, String tablename) {
 
         try {
             table = conn.getTable(TableName.valueOf(tablename));
-
+            this.columnFamilies = new AdminUtils(conn).columnFamilies(tablename) ;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,59 +39,105 @@ public class HBaseConsumer {
 
 
 
-    public List<Quartet<String,String,String,String>> getRecords(String key, List<Pair<String,String>> famQuals){
-        Get get = new Get(key.getBytes()) ;
-        for(Pair<String,String> fq: famQuals){
-            get=get.addColumn(fq.getFirst().getBytes(),fq.getSecond().getBytes());
+    public List<Quartet<String, String, String, String>> getRecords(String key, List<Pair<String, String>> famQuals) {
+        Get get = new Get(key.getBytes());
+        for (Pair<String, String> fq : famQuals) {
+            if (this.columnFamilies.contains(fq.getFirst())){
+                get = get.addColumn(fq.getFirst().getBytes(), fq.getSecond().getBytes());
+            }
         }
-
         try {
-            return processResults(this.table.get(get)) ;
+            return processResults(this.table.get(get));
+        } catch (Exception e) {
+            return null;
         }
-        catch(Exception e){
+    }
+
+    public Quartet<String, String, String, String> getRecord(String key, String family, String qualifier) {
+        if (!this.columnFamilies.contains(family)){
             return null ;
         }
-    }
-
-    public Quartet<String,String,String,String> getRecord(String key, String family, String qualifier) {
-        List<Pair<String,String>> famQuals = new ArrayList<>() ;
-        famQuals.add(new Pair<>(family,qualifier));
-        try{
-            return getRecords(key, famQuals).get(0) ;
-        }
-        catch (Exception e){
-            return null ;
+        List<Pair<String, String>> famQuals = new ArrayList<>();
+        famQuals.add(new Pair<>(family, qualifier));
+        try {
+            return getRecords(key, famQuals).get(0);
+        } catch (Exception e) {
+            return null;
         }
     }
 
-    public Quartet<String,String,String,String> scan(){
-        return null ;
-    }
+    public List<Quartet<String, String, String, String>> scan(String startKey,
+                                                              String endKey,
+                                                              List<Pair<String, String>> famQuals,
+                                                              List<Filter> filters) {
+        Scan s = new Scan(startKey.getBytes(),endKey.getBytes()) ;
+        //adding columns to the scan
+        if (famQuals != null){
+            for (Pair<String, String> fq : famQuals) {
+                if (this.columnFamilies.contains(fq.getFirst())){
+                    s = s.addColumn(fq.getFirst().getBytes(), fq.getSecond().getBytes());
+                }
+            }
+        }
+        // adding filters to the scan
+        if (filters != null){
+            for(Filter f: filters){
+                s = s.setFilter(f) ;
+            }
+        }
+        try {
+            return processResultScanner(table.getScanner(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.<Quartet<String, String, String, String>>emptyList() ;
+        }
 
-    private List<Quartet<String,String,String,String>> processResults(Result result){
+    }
+    private List<Quartet<String, String, String, String>> processResultScanner(ResultScanner scanner){
+        List<Quartet<String, String, String, String>> results = new ArrayList<>() ;
+        try  {
+            for (Result rr = scanner.next(); rr != null; rr = scanner.next()) {
+                results.addAll(processResults(rr));
+            }
+            return results ;
+        } catch (IOException e) {
+            e.printStackTrace();
+            results = Collections.<Quartet<String, String, String, String>>emptyList() ;
+        }
+        return results ;
+    }
+    private List<Quartet<String, String, String, String>> processResults(Result result) {
         CellScanner scanner = result.cellScanner();
-        List<Quartet<String,String,String,String>> qrts = new ArrayList<>() ;
+        List<Quartet<String, String, String, String>> qrts = new ArrayList<>();
         try {
             while (scanner.advance()) {
                 Cell cell = scanner.current();
                 qrts.add(new Quartet<>(new String(CellUtil.cloneRow(cell), StandardCharsets.UTF_8),
                         new String(CellUtil.cloneFamily(cell), StandardCharsets.UTF_8),
                         new String(CellUtil.cloneQualifier(cell), StandardCharsets.UTF_8),
-                        new String(CellUtil.cloneValue(cell), StandardCharsets.UTF_8))) ;
+                        new String(CellUtil.cloneValue(cell), StandardCharsets.UTF_8)));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return qrts ;
+        return qrts;
     }
 
     public static void main(String[] args) throws Exception {
         HBaseConsumer g = new HBaseConsumer(new HBaseConnectionBuilder("quickstart.cloudera:2181").build(), "t1");
-        List<Pair<String,String>> famQuals = new ArrayList<>() ;
+        List<Filter> filters = new ArrayList<>() ;
+        ColumnPrefixFilter colFil = new ColumnPrefixFilter(Bytes.toBytes("acol")) ;
+        filters.add(colFil);
+        List<Quartet<String, String, String, String>> scan = g.scan("k", "l",null,filters);
+        System.out.println(scan.size());
+        for(Quartet<String, String, String, String> a : scan){
+            System.out.println(a) ;
+        }
+
+        /*
+        List<Pair<String, String>> famQuals = new ArrayList<>();
         famQuals.add(new Pair<>("cf1", "col1"));
         System.out.println(g.getRecords("key1", famQuals));
-
+        */
     }
-
-
 }
